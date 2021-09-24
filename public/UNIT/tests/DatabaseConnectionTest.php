2f042d9777381c05c3cc154254e693de2a768742
fmarabini93 <fmarabini@icloud.com>
2021-09-24 12:52:32 +0200

    db_unit_test

--- public/UNIT/tests/DatabaseConnectionTest.php
+++ public/UNIT/tests/DatabaseConnectionTest.php
@@ -3,14 +3,13 @@
 use PHPUnit\Framework\TestCase;
 use App\Database;
 
-use function PHPUnit\Framework\objectEquals;
-
-class DatabaseTest extends TestCase
+class DatabaseConnectionTest extends TestCase
 {
-      protected $server_name = "localhost";
-      protected $username = "root";
-      protected $password = "";
-      protected $db_name = "edu_test";
+      private $server_name = "localhost";
+      private $username = "root";
+      private $password = "";
+      private $db_name = "edu_test";
+      
       /** @test */
       public function canEstablishConnection() {
             $db = new Database;
--- public/UNIT/tests/UserTest.php
+++ public/UNIT/tests/UserTest.php
@@ -5,62 +5,62 @@ use App\User;
 
 class UserTest extends TestCase
 {     
+      protected $user;
+
+      public function setUp() : void {
+            $this->user = new User;
+      }
+
       /** @test */
       public function canGetName() {
-            $user = new User;
 
-            $user->setName('Johnny');
+            $this->user->setName('Johnny');
 
-            $this->assertEquals($user->getName(), 'Johnny');
+            $this->assertEquals($this->user->getName(), 'Johnny');
       }
 
       /** @test */
       public function canGetSurname() {
-            $user = new User;
 
-            $user->setSurname('Bianchi');
+            $this->user->setSurname('Bianchi');
 
-            $this->assertEquals($user->getSurname(), 'Bianchi');
+            $this->assertEquals($this->user->getSurname(), 'Bianchi');
       }
 
       /** @test */
       public function canGetFullName() {
-            $user = new User;
 
-            $user->setName('Johnny');
-            $user->setSurname('Bianchi');
+            $this->user->setName('Johnny');
+            $this->user->setSurname('Bianchi');
 
-            $this->assertEquals($user->getFullName(), 'Johnny Bianchi');
+            $this->assertEquals($this->user->getFullName(), 'Johnny Bianchi');
       }
 
       /** @test */
       public function canGetEmail() {
-            $user = new User;
 
-            $user->setEmail('bianchi@johnny.com');
+            $this->user->setEmail('bianchi@johnny.com');
 
-            $this->assertEquals($user->getEmail(), 'bianchi@johnny.com');
+            $this->assertEquals($this->user->getEmail(), 'bianchi@johnny.com');
       }
 
       /** @test */
       public function canGetFullUser() {
-            $user = new User;
 
-            $user->setName('Johnny');
-            $user->setSurname('Bianchi');
-            $user->setEmail('bianchi@johnny.com');
+            $this->user->setName('Johnny');
+            $this->user->setSurname('Bianchi');
+            $this->user->setEmail('bianchi@johnny.com');
 
-            $this->assertEquals($user->getFullUser(), 'Johnny Bianchi bianchi@johnny.com');
+            $this->assertEquals($this->user->getFullUser(), 'Johnny Bianchi bianchi@johnny.com');
       }
 
       /** @test */
       public function verifyTrimming() {
-            $user = new User;
 
-            $user->setName('      Johnny');
-            $user->setSurname('Bianchi      ');
+            $this->user->setName('      Johnny');
+            $this->user->setSurname('Bianchi      ');
 
-            $this->assertEquals($user->getName(), 'Johnny');
-            $this->assertEquals($user->getSurname(), 'Bianchi');
+            $this->assertEquals($this->user->getName(), 'Johnny');
+            $this->assertEquals($this->user->getSurname(), 'Bianchi');
       }
 }
